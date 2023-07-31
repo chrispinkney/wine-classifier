f4b8c74f6d97823a8990ef0f935a351f1b8ab3b7
Chris Pinkney <hey@chrispinkney.com>
2023-07-31 16:39:14 -0400

    suppress warnings, alter confusion matrix

--- wine.py
+++ wine.py
@@ -1,9 +1,13 @@
 import time
 import numpy as np
+import warnings
 from sklearn.svm import LinearSVC
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings('ignore', category=FutureWarning)
+warnings.simplefilter(action='ignore', category=ConvergenceWarning)
 
 
 def svm(X_train, y_train, X_test, y_test):
@@ -27,10 +31,13 @@ def svm(X_train, y_train, X_test, y_test):
 
     # Display the confusion matrix to evaluate classification performance
     y_true = y_test  # True class labels for the test set
-    conf_matrix = confusion_matrix(y_true, y_pred)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
 
     print("SVM Results:")
-    print(f"Confusion Matrix: {conf_matrix}")
+    print(f"True Positives: {tp}")
+    print(f"True Negatives: {tn}")
+    print(f"False Positives: {fp}")
+    print(f"False Negatives: {fn}")
     print(f"Elapsed Time: {elapsed_time * 1000:.2f} ms")
     print(f"Accuracy: {accuracy * 100:.2f}%")
     print(f"Total time to run program: {elapsed_time:.2f} seconds")
@@ -57,28 +64,31 @@ def fishers(X_train, y_train, X_test, y_test):
 
     # Display the confusion matrix to evaluate classification performance
     y_true = y_test  # True class labels for the test set
-    conf_matrix = confusion_matrix(y_true, y_pred)
+    tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
 
     print("\nFisher's Linear Discriminant Results:")
-    print(f"Confusion Matrix: {conf_matrix}")
+    print(f"True Positives: {tp}")
+    print(f"True Negatives: {tn}")
+    print(f"False Positives: {fp}")
+    print(f"False Negatives: {fn}")
     print(f"Elapsed Time: {elapsed_time * 1000:.2f} ms")
     print(f"Accuracy: {accuracy * 100:.2f}%")
     print(f"Total time to run program: {elapsed_time:.2f} seconds")
 
 
 # Load red and white wine data
-rawTrainingDataRed = np.genfromtxt('winequality-red.csv', delimiter=';', skip_header=1, max_rows=1000)
-rawTrainingDataWhite = np.genfromtxt('winequality-white.csv', delimiter=';', skip_header=1, max_rows=1000)
+raw_training_data_red = np.genfromtxt('winequality-red.csv', delimiter=';', skip_header=1, max_rows=1000)
+raw_training_data_white = np.genfromtxt('winequality-white.csv', delimiter=';', skip_header=1, max_rows=1000)
 
 # Create labels for training data: 0 for red wine, 1 for white wine
-red_labels = np.zeros((rawTrainingDataRed.shape[0], 1))
-white_labels = np.ones((rawTrainingDataWhite.shape[0], 1))
+red_labels = np.zeros((raw_training_data_red.shape[0], 1))
+white_labels = np.ones((raw_training_data_white.shape[0], 1))
 
 # Combine red and white wine data into one array
-combinedData = np.concatenate((rawTrainingDataRed, rawTrainingDataWhite), axis=0)
+combined_data = np.concatenate((raw_training_data_red, raw_training_data_white), axis=0)
 
 # Prepare attributes (X) and classes (y) from the combined data
-X = combinedData[:, :11]  # Extract the first 11 columns as attributes
+X = combined_data[:, :11]  # Extract the first 11 columns as attributes
 y = np.vstack((red_labels, white_labels)).flatten() # Combine the class labels for red and white wine data into a single one-dimensional array 'y'
 
 # Split data into training and testing sets
