5e4d205be24b8baa36cdedd007f64a3aff47154a
Chris Pinkney <hey@chrispinkney.com>
2023-07-31 15:07:53 -0400

    implement fishers

---
+++ README.md
@@ -0,0 +1,3 @@
+# Running the code
+- `pip install -r requirements.txt`
+- `python wine.py`
--- wine.py
+++ wine.py
@@ -1,11 +1,12 @@
 import time
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix
 
 
-def svm(x_train, y_train, x_test, y_test):
+def svm(X_train, y_train, X_test, y_test):
 	# Create an instance of the LinearSVC classifier
 	clf = LinearSVC()
 
@@ -13,16 +14,51 @@ def svm(x_train, y_train, x_test, y_test):
 	start_time = time.time()
 
 	# Train the LinearSVC classifier
-	clf.fit(x_train, y_train)
+	clf.fit(X_train, y_train)
 
 	# Perform predictions on the test set
-	y_pred = clf.predict(x_test)
+	y_pred = clf.predict(X_test)
 
 	# End timing the training process
 	elapsed_time = time.time() - start_time
 
 	# Evaluate the accuracy of the classifier on the test set
-	accuracy = clf.score(x_test, y_test)
+	accuracy = clf.score(X_test, y_test)
+
+	# Display the confusion matrix to evaluate classification performance
+	y_true = y_test  # True class labels for the test set
+	conf_matrix = confusion_matrix(y_true, y_pred)
+
+	print("Confusion Matrix:", conf_matrix)
+
+	# Display the elapsed time for training in milliseconds
+	print("\nElapsed Time:", elapsed_time * 1000, "ms")
+
+	# Display the accuracy of the classifier as a percentage
+	print("Accuracy:", accuracy * 100, "%")
+
+	# Display the total time taken to run the entire program
+	end = time.time()
+	print("Total time to run program:", end - start_time)
+
+
+def fishers(X_train, y_train, X_test, y_test):
+	clf_fld = LDA()
+
+	# Start timing the training process
+	start_time = time.time()
+
+	# Train the LinearSVC classifier
+	clf_fld.fit(X_train, y_train)
+
+	# Perform predictions on the test set
+	y_pred = clf_fld.predict(X_test)
+
+	# End timing the training process
+	elapsed_time = time.time() - start_time
+
+	# Evaluate the accuracy of the classifier on the test set
+	accuracy = clf_fld.score(X_test, y_test)
 
 	# Display the confusion matrix to evaluate classification performance
 	y_true = y_test  # True class labels for the test set
@@ -45,6 +81,7 @@ def svm(x_train, y_train, x_test, y_test):
 rawTrainingDataRed = np.genfromtxt('winequality-red.csv', delimiter=';', skip_header=1, max_rows=1000)
 rawTrainingDataWhite = np.genfromtxt('winequality-white.csv', delimiter=';', skip_header=1, max_rows=1000)
 
+# Create red and white labels for training
 red_labels = np.zeros((rawTrainingDataRed.shape[0], 1))
 white_labels = np.ones((rawTrainingDataWhite.shape[0], 1))
 
@@ -53,9 +90,10 @@ combinedData = np.concatenate((rawTrainingDataRed, rawTrainingDataWhite), axis=0
 
 # Prepare attributes (X) and classes (y) from the combined data
 X = combinedData[:, :11]  # Extract the first 11 columns as attributes
-y = np.vstack((red_labels, white_labels)).flatten()
+y = np.vstack((red_labels, white_labels)).flatten() # Combine the class labels for red wine data (zeros) and white wine data (ones) into a single one-dimensional array 'y'
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 svm(X_train, y_train, X_test, y_test)
+fishers(X_train, y_train, X_test, y_test)
